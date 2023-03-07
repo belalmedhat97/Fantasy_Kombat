f19cbc8f52d79b7f6fb4e4f4d60e69fbd25f3677
Belal <belal1997medhat@gmail.com>
2023-03-07 00:24:03 +0000

    refactor func readability and made some code changes

--- NewFantasyGame.playground/Contents.swift
+++ NewFantasyGame.playground/Contents.swift
@@ -7,23 +7,26 @@ class Logger{
         print(message)
     }
 }
+
 enum LoggerLabels {
     static let endgame = "endgame"
     static let startGame = "start game \n"
     static func getRoundCounter(number: String) -> String {
         return "round \(number) \n"
     }
+    
     static func getDamage(playerName: String, playerInflicts:String) -> String {
         return ("\(playerName) inflicts \(playerInflicts) of damage")
     }
+    
     static func getLeftLife(playerName: String, lifeLeft: String) -> String {
         return ("\(playerName) has \(lifeLeft) points of life left")
     }
+    
     static func getWinnerPlayer(playerName: String) -> String {
         return"\(playerName) wins !!!"
     }
 }
-
 
 class Player{
     var name:String
@@ -43,55 +46,77 @@ class Player{
 
 }
 
- 
-class Attack{
-    private var normalConstant = 1
-    private var missConstant = 0
-    private var criticalConstant = 3
-    
-    private var percentage = ["normal":0.6,"miss":0.2,"critical":0.2]
-    
-     func returnRandomAttack() -> String {
-        // get a random attack type regarding to the weight of propability of it
-        var percantageProbilities: [Double] = [0.6,0.2,0.2]
-        let sumPercantage = percantageProbilities.reduce(0, +)
-        var randomProbailityPercantageRange = Double.random(in: 0.0 ..< sumPercantage)
+protocol AttackType {
+    var damageMultiplier: Int { get }
+    static var chances: Int { get }
+    var label: String { get }
+}
 
-        var sum = 0.0
-        for (attack, weight) in percentage {
-            sum += weight
-            if randomProbailityPercantageRange < weight {
-                return attack
-            }
-            
+class Normal: AttackType {
+    let damageMultiplier = 1
+    static let chances = 60
+    var label = "normal"
+}
+
+class Miss: AttackType {
+    let damageMultiplier = 0
+    static let chances = 20
+    let label = "miss"
+
+}
+
+class Critical: AttackType {
+    let damageMultiplier = 3
+    static let chances = 20
+    let label = "critical"
+
+}
+
+class AttackTypeFactory {
+    func create(luckBonus: Int) -> AttackType {
+        let start = 0
+        let missLimit = start + Miss.chances
+        let normalLimit = missLimit + Normal.chances
+        let criticalLimit = normalLimit + Critical.chances + luckBonus
+        let random = Int.random(in: start...criticalLimit)
+
+        switch random {
+        case start..<missLimit:
+            return Miss()
+        case missLimit..<normalLimit:
+            return Normal()
+        case normalLimit...criticalLimit:
+            return Critical()
+        default:
+            return Normal()
         }
-        return "normal"
-        
     }
-    func returnAttackConstant(attack: String) -> Int {
-        switch attack {
-        case "normal":
-            return normalConstant
-        case "miss":
-            return missConstant
-        default :
-            return criticalConstant
-        }
     }
-    func returnPlayerRemainLife(playerLife: Int, Inflicts: Int) -> Int {
+
+
+class Attack{
+
+    func calculatePlayerRemainLife(playerLife: Int, Inflicts: Int) -> Int {
         // check if life remain will be less than zero then return zero and life ends for the player
         let remainLife = playerLife - Inflicts
+        
         if  remainLife.signum() == -1 {
             return 0
-        }else{
+        } else {
             return remainLife
         }
     }
-    func getTotalAttack(playerAttack: Int, attackType: String) -> Int {
-        return ( playerAttack * returnAttackConstant(attack: attackType))
+    
+    func calculateTotalAttack(playerAttack: Int, damage: Int) -> Int {
+        return playerAttack * damage
     }
-    func getInflicts(totalAttack: Int, defence: Int ) -> Int {
-       return (totalAttack - defence)
+    
+    func calculateInflicts(totalAttack: Int, defence: Int ) -> Int {
+        var inflicts = (totalAttack - defence)
+        if  inflicts.signum() == -1  {
+            inflicts = 0
+        }
+       return inflicts
     }
 
     func getStarterPlayer(player1:Player, player2:Player) -> Player {
@@ -114,20 +139,17 @@ class Attack{
 
 class Battle{
     private var roundCounter:Int = 0
-    private var playersExchangedAttacks = false
     private let attack:Attack = Attack()
-    private var playerToStart = Player1
-    private var player1:Player
-    private var player2:Player
+    private var player1: Player
+    private var player2: Player
     init(player1: Player, player2: Player) {
         self.player1 = player1
         self.player2 = player2
         Logger.shared.returnLog(message:"start game \n")
         beginRound()
-        playerToStart = attack.getStarterPlayer(player1: player1, player2: player2)
     }
     private func beginRound() {
-        playersExchangedAttacks = false
+        let playerToStart = attack.getStarterPlayer(player1: player1, player2: player2)
         showRound()
         beginFight(firstPlayer: playerToStart, secondPlayer: playerToStart === self.player1 ? self.player2 : self.player1)
         if checkGameWillEnd() != true {
@@ -135,83 +157,71 @@ class Battle{
         }
     }
     
+    private func showRound() {
+        roundCounter += 1
+        Logger.shared.returnLog(message:"\n")
+        Logger.shared.returnLog(message:"round \(roundCounter)")
+    }
 
-    
-    private func beginFight(firstPlayer: Player,secondPlayer: Player) {
-        let attackType = attack.returnRandomAttack()
-        if attackType != "normal" {
-            Logger.shared.returnLog(message:attackType)
-        }
-        // inflicts : ( (total = attack * attackTypesConstant) - Defence )
-        let totalAttack = attack.getTotalAttack(playerAttack: firstPlayer.attack, attackType: attackType)
+    private func beginFight(firstPlayer: Player, secondPlayer: Player) {
+        let attackType = AttackTypeFactory().create(luckBonus: firstPlayer.luck)
+        showAttackType(type: attackType)
         
-        var playerInflicts = attack.getInflicts(totalAttack: totalAttack, defence: secondPlayer.defence)
-        // lifeLeft : ( life - playerInflicts )
-        if checkInflictsWillBeNegaitve(inflicts: playerInflicts) == true {
-            playerInflicts = 0
-        }
+        let totalAttack = attack.calculateTotalAttack(playerAttack: firstPlayer.attack, damage: attackType.damageMultiplier)
         
-        let lifeLeft = attack.returnPlayerRemainLife(playerLife: secondPlayer.life, Inflicts: playerInflicts)
+        var playerInflicts = attack.calculateInflicts(totalAttack: totalAttack, defence: secondPlayer.defence)
+
+        let lifeLeft = attack.calculatePlayerRemainLife(playerLife: secondPlayer.life, Inflicts: playerInflicts)
         Logger.shared.returnLog(message: LoggerLabels.getDamage(playerName: firstPlayer.name, playerInflicts: "\(playerInflicts)"))
         Logger.shared.returnLog(message: LoggerLabels.getLeftLife(playerName: secondPlayer.name, lifeLeft: "\(lifeLeft)"))
+        
         checkPlayerWillAttackAgainOrNot(firstPlayer: firstPlayer, secondPlayer: secondPlayer, lifeLeft: lifeLeft)
 
-
     }
-    private func returnNextPlayerToStart(toStartPlayer: Player, lifeleft: Int) -> Player {
-        if toStartPlayer === player1 {
-            player1.life = lifeleft
-            return player1
-        }else{
-            player2.life = lifeleft
-            return player2
 
+    private func showAttackType(type:AttackType) {
+        if type.label != "normal" {
+            Logger.shared.returnLog(message:type.label)
         }
     }
+    
     private func checkPlayerWillAttackAgainOrNot(firstPlayer: Player,secondPlayer: Player, lifeLeft: Int) {
         let nextPlayer = returnNextPlayerToStart(toStartPlayer: secondPlayer, lifeleft: lifeLeft)
         if lifeLeft == 0 {
             Logger.shared.returnLog(message:LoggerLabels.getWinnerPlayer(playerName:firstPlayer.name))
-        }else{
-            if playersExchangedAttacks != true {
-                playersExchangedAttacks = true
+        } else {
+            if attack.getStarterPlayer(player1: player1, player2: player2) === firstPlayer {
                 beginFight(firstPlayer: nextPlayer, secondPlayer: firstPlayer)
             }
             
         }
     }
-    private func checkInflictsWillBeNegaitve(inflicts: Int) -> Bool {
-        // check if damage will negative then return damage to be zero
-        if  inflicts.signum() == -1  {
-            return true
-        }else{
-            return false
+    
+    private func returnNextPlayerToStart(toStartPlayer: Player, lifeleft: Int) -> Player {
+        if toStartPlayer === player1 {
+            player1.life = lifeleft
+            return player1
+        } else {
+            player2.life = lifeleft
+            return player2
+
         }
-    }
-    private func showRound() {
-        roundCounter += 1
-        Logger.shared.returnLog(message:"\n")
-        Logger.shared.returnLog(message:"round \(roundCounter)")
     }
     
     private func checkGameWillEnd() -> Bool {
         if player1.life == 0 {
             Logger.shared.returnLog(message: "END GAME")
             return true
-        }else if player2.life == 0 {
+        } else if player2.life == 0 {
             Logger.shared.returnLog(message: "END GAME")
             return true
         }
         return false
     }
-
     
-
-    
-
 }
 
-var Player1 = Player(name: "Midoriya", attack: 10, defence: 3, speed: 50, life: 35, luck: 50)
+var Player1 = Player(name: "Midoriya", attack: 10, defence: 3, speed: 50, life: 35, luck: 0)
 var Player2 = Player(name: "Bakugo", attack: 10, defence: 5, speed: 40, life: 50, luck: 0)
 let battle = Battle(player1: Player1, player2: Player2)
 
